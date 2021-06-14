f03f73dfbd93f41da430dfd42c4aeb30b4fc760f
sakogan1 <samir.kogan11@gmail.com>
2021-06-14 19:08:34 +0200

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import "../css/Footer.css"
+
+const Footer = () => {
+    return ( 
+<div id="main">
+
+<div id="One"><div id="image"></div><h1>Lorem ipsum</h1><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent.</p><h2>$45/55</h2>   </div>
+<div id="Two"><div id="image"></div><h1>Lorem ipsum</h1><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent.</p><h2>$65/70</h2>   </div>
+<div id="Three"><div id="image"></div><h1>Lorem ipsum</h1><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent.</p><h2>$30.50</h2>   </div>
+<div id="Four"><div id="image"></div><h1>Lorem ipsum</h1><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent.</p><h2>$25.50</h2>   </div>
+<div id="Five"><div id="image"></div><h1>Lorem ipsum</h1><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent.</p>   </div>
+<div id="Six"><div id="image"></div><h1>Lorem ipsum</h1><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent.</p>   </div>
+<div id="Seven"><div id="image"></div><h1>Lorem ipsum</h1><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent.</p>   </div>
+<div id="Eight"><div id="image"></div><h1>Lorem ipsum</h1><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent.</p>   </div>
+
+
+
+
+</div>
+     );
+}
+ 
+export default Footer;
---
+++ src/components/Head.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+import "../css/Head.css"
+
+const Head = () => {
+    return ( 
+        <header>
+<h2>Hello There</h2>
+<h1>We Are Glint</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur 
+   augue eget aliquam commodo. Sed venenatis tincidunt libero ut pretium. Pellentesque quis vestibulum turpis. Donec vitae urna quis augue fringilla rhoncus eu eu metus. Aenean iaculis dolor lacinia rutrum bibendum. Phasellus imperdiet libero non commodo bibendum. Praesent felis quam,
+   porttitor vitae erat vitae, feugiat tristique dui. Ut in lacus elit.
+</p>
+<div id="headerDiv">
+<h3>127<p>Award Recieved</p></h3><div></div><h3>1505<p>Cups of Coffee</p></h3><div></div><h3>109 <p>Projects Completed</p></h3><div></div><h3>102<p>Happy Clients</p></h3>
+</div>
+        </header>
+
+
+     );
+}
+ 
+export default Head;
